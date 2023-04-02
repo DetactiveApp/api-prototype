0da091d005e1ae72b6d83f4ac127e306d2c31c82
Max Beier <max@maxbeier.dev>
2023-04-02 19:39:34 +0200

    Update db.rs

--- api_server/src/utils/db.rs
+++ api_server/src/utils/db.rs
@@ -5,7 +5,7 @@ use redis::Commands;
 use redis::Connection;
 
 pub fn user_db_conn() -> RedisResult<Connection> {
-    let _client = redis::Client::open("redis://user-db:6379");
+    let _client = redis::Client::open("redis://127.0.0.1:6379");
     match _client {
         Ok(client) => return client.get_connection(),
         Err(err) => return Err(err),
@@ -13,7 +13,7 @@ pub fn user_db_conn() -> RedisResult<Connection> {
 }
 
 pub fn blueprint_db_conn() -> RedisResult<Connection> {
-    let _client = redis::Client::open("redis://blueprint-db:6380");
+    let _client = redis::Client::open("redis://127.0.0.1:6380");
     match _client {
         Ok(client) => return client.get_connection(),
         Err(err) => return Err(err),
