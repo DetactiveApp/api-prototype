91422cd88fa6189575ae114f4da1d581c3a7f9cc
Maccci <max@maxbeier.dev>
2023-04-29 23:35:40 +0200

    test

--- api/src/utils/db.rs
+++ api/src/utils/db.rs
@@ -1,26 +1,16 @@
-use sqlx::postgres::{PgPool, PgPoolOptions};
+use sqlx::postgres::PgPool;
 
-const DETACTIVE_DB: &str = "postgresql://postgres:b814dc1ee44c62b78d1b48e3ff2a43effee817b234fdc0de75db05a5013aaecf@0.0.0.0:5432/detactive";
-const STICKER_DB: &str = "postgresql://postgres:b814dc1ee44c62b78d1b48e3ff2a43effee817b234fdc0de75db05a5013aaecf@0.0.0.0:5432/sticker";
+const DETACTIVE_DB: &str = "postgresql://postgres:b814dc1ee44c62b78d1b48e3ff2a43effee817b234fdc0de75db05a5013aaecf@127.0.0.1:5432/detactive";
+const STICKER_DB: &str = "postgresql://postgres:b814dc1ee44c62b78d1b48e3ff2a43effee817b234fdc0de75db05a5013aaecf@127.0.0.1/sticker";
 
 pub async fn detactive_pool() -> PgPool {
-    let pool = PgPoolOptions::new()
-        .max_connections(50)
-        .connect(DETACTIVE_DB)
-        .await
-        .unwrap();
-
+    let pool = PgPool::connect(DETACTIVE_DB).await.unwrap();
     sqlx::migrate!("db/detactive").run(&pool).await.unwrap();
     return pool;
 }
 
 pub async fn sticker_pool() -> PgPool {
-    let pool = PgPoolOptions::new()
-        .max_connections(50)
-        .connect(STICKER_DB)
-        .await
-        .unwrap();
-
+    let pool = PgPool::connect(STICKER_DB).await.unwrap();
     sqlx::migrate!("db/sticker").run(&pool).await.unwrap();
     return pool;
 }
