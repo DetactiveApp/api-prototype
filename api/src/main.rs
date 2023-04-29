17da11ed6f6850d1228e1cd1fc218ff3297c14d4
Maccci <max@maxbeier.dev>
2023-04-30 00:24:11 +0200

    removed pool extensions for now
    *planned to add them later in the specfifc tree

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,10 +2,9 @@ mod routes;
 mod types;
 mod utils;
 
-use axum::{Extension, Router};
+use axum::Router;
 
 use reqwest::{header, Method};
-use sqlx::PgPool;
 use tower_http::cors::{Any, CorsLayer};
 
 use std::{net::SocketAddr, time::Duration};
@@ -33,13 +32,8 @@ async fn main() {
         .allow_origin(Any)
         .max_age(Duration::from_secs(60 * 60));
 
-    let detactive_db_pool: PgPool = utils::db::detactive_pool().await;
-    let sticker_db_pool: PgPool = utils::db::sticker_pool().await;
-
     let app = Router::new()
         .nest(&format!("/api/v{}", &env!("CARGO_PKG_VERSION")[..1]), api())
-        .layer(Extension(detactive_db_pool))
-        .layer(Extension(sticker_db_pool))
         .layer(cors);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
--- api/src/utils/db.rs
+++ api/src/utils/db.rs
@@ -1,16 +1,24 @@
-use sqlx::postgres::PgPool;
+use sqlx::postgres::{PgPool, PgPoolOptions};
 
 const DETACTIVE_DB: &str = "postgresql://postgres:b814dc1ee44c62b78d1b48e3ff2a43effee817b234fdc0de75db05a5013aaecf@127.0.0.1:5432/detactive";
 const STICKER_DB: &str = "postgresql://postgres:b814dc1ee44c62b78d1b48e3ff2a43effee817b234fdc0de75db05a5013aaecf@127.0.0.1/sticker";
 
 pub async fn detactive_pool() -> PgPool {
-    let pool = PgPool::connect(DETACTIVE_DB).await.unwrap();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(DETACTIVE_DB)
+        .await
+        .unwrap();
     sqlx::migrate!("db/detactive").run(&pool).await.unwrap();
     return pool;
 }
 
 pub async fn sticker_pool() -> PgPool {
-    let pool = PgPool::connect(STICKER_DB).await.unwrap();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(STICKER_DB)
+        .await
+        .unwrap();
     sqlx::migrate!("db/sticker").run(&pool).await.unwrap();
     return pool;
 }
