a6c43ec0e476905a04bc5cd9d3d073185d07dd83
Maccci <max@maxbeier.dev>
2023-04-29 12:30:26 +0200

    #19 Migrated Migrations from ServerV1 Repo  to Server Repo

---
+++ api/migrations/20230423124050_initial_schema.down.sql
@@ -0,0 +1,33 @@
+/***************************************************
+**-------------- REMOVE CONSTRAINTS --------------**
+***************************************************/
+
+ALTER TABLE decisions 
+DROP CONSTRAINT fk_step_input, 
+DROP CONSTRAINT fk_step_output;
+
+ALTER TABLE player_stories 
+DROP CONSTRAINT fk_story, 
+DROP CONSTRAINT fk_player;
+
+ALTER TABLE player_story_steps 
+DROP CONSTRAINT fk_player_story, 
+DROP CONSTRAINT fk_step;
+
+/***************************************************
+**----------------- REMOVE TABLES ----------------**
+***************************************************/
+
+DROP TABLE decisions, players, player_stories, player_story_steps, steps, stories;
+
+/***************************************************
+**-------------- REMOVE CUSTOM TYPES -------------**
+***************************************************/
+
+DROP TYPE IF EXISTS MEDIUMTYPE;
+
+/***************************************************
+**--------------- REMOVE EXTENSIONS --------------**
+***************************************************/
+
+DROP EXTENSION IF EXISTS "uuid-ossp";
