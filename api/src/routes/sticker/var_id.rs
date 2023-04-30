1944d60089c972e231d909989c8049bcb170ba20
Maccci <max@maxbeier.dev>
2023-04-30 21:09:02 +0200

    efficient request

--- api/src/routes/sticker/var_id.rs
+++ api/src/routes/sticker/var_id.rs
@@ -7,19 +7,12 @@ use crate::utils::db;
 pub async fn request(Path(path): Path<String>) -> Result<String, StatusCode> {
     let db: sqlx::Pool<sqlx::Postgres> = db::sticker_pool().await;
 
-    let stickers = sqlx::query("SELECT id, redirect_url FROM stickers WHERE deleted_at IS NULL;")
-        .fetch_all(&db)
+    match sqlx::query("SELECT redirect_url FROM stickers WHERE deleted_at IS NULL AND id = $1;")
+        .bind(&path)
+        .fetch_one(&db)
         .await
-        .unwrap();
-
-    for sticker in stickers {
-        let sticker_id: String = sticker.get("id");
-        let redirect_url: String = sticker.get("redirect_url");
-
-        if sticker_id == path {
-            return Ok(redirect_url);
-        }
-    }
-
-    return Err(StatusCode::NOT_FOUND);
+    {
+        Ok(sticker) => return Ok(sticker.get("redirect_url")),
+        Err(_) => return Err(StatusCode::NOT_FOUND),
+    };
 }
