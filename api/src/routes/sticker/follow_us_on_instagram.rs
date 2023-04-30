13a5c9834baa3054282101e3fdeaa0af3812e376
Maccci <max@maxbeier.dev>
2023-04-30 15:20:34 +0200

    restructure

--- api/src/routes/sticker/follow_us_on_instagram.rs
+++
@@ -1,6 +0,0 @@
-use axum::Extension;
-use sqlx::PgPool;
-
-pub async fn request() -> String {
-    return String::from("https://www.instagram.com/detactive.de/");
-}
--- api/src/routes/sticker/mod.rs
+++ api/src/routes/sticker/mod.rs
@@ -1,10 +1,13 @@
-use axum::{routing::get, Extension, Router};
-
-mod follow_us_on_instagram;
-use follow_us_on_instagram::request;
+use axum::{
+    routing::{get, Route},
+    Router,
+};
 
 use crate::utils::db;
 
 pub async fn sticker_router() -> Router {
-    return Router::new().route("/follow-us-on-instagram", get(request));
+    let detactive_db_pool = db::detactive_pool().await;
+    let mut router = Router::new();
+
+    return router;
 }
