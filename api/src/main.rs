fb306e184c6380a3b5fa4d0a87c28c5a1b59883c
Maccci <max@maxbeier.dev>
2023-05-04 22:06:10 +0200

    nginx gateway config

--- api/src/main.rs
+++ api/src/main.rs
@@ -6,14 +6,10 @@ use std::net::SocketAddr;
 
 use crate::{routes::api, utils::cors};
 use axum::Router;
-use axum_server::tls_rustls::RustlsConfig;
 
 #[tokio::main]
 async fn main() {
     println!("Detactive API v{}", env!("CARGO_PKG_VERSION"));
-
-    let domain =
-        std::env::var("ACME_DOMAIN").unwrap_or_else(|_| "test-api.detactive.de".to_string());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let app = Router::new()
@@ -23,23 +19,8 @@ async fn main() {
         )
         .layer(cors());
 
-    match RustlsConfig::from_pem_file(
-        format!("/var/www/{}/cert.pem", domain),
-        format!("/var/www/{}/key.pem", domain),
-    )
-    .await
-    {
-        Ok(config) => {
-            axum_server::bind_rustls(addr, config)
-                .serve(app.into_make_service())
-                .await
-                .unwrap();
-        }
-        Err(_) => {
-            axum_server::bind(addr)
-                .serve(app.into_make_service())
-                .await
-                .unwrap();
-        }
-    }
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
