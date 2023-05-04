2e24985b9a2638534dfe65e9c00277d471c8a9e8
Maccci <max@maxbeier.dev>
2023-05-04 17:28:02 +0200

    [DEBUG] added certbot

--- api/src/main.rs
+++ api/src/main.rs
@@ -2,31 +2,17 @@ mod routes;
 mod types;
 mod utils;
 
-use std::{net::SocketAddr, path::PathBuf, sync::Arc};
+use std::net::SocketAddr;
 
 use crate::{routes::api, utils::cors};
 use axum::Router;
-use rustls_acme::{caches::DirCache, futures_rustls::rustls::ServerConfig, AcmeConfig};
+use axum_server::tls_rustls::RustlsConfig;
 
 #[tokio::main]
 async fn main() {
     println!("Detactive API v{}", env!("CARGO_PKG_VERSION"));
 
-    let state = AcmeConfig::new(vec!["test-api.detactive.de"])
-        .contact(vec!["mailto: max@maxbeier.dev"])
-        .cache_option(Some(PathBuf::new().clone()).map(DirCache::new))
-        .directory_lets_encrypt(true)
-        .state();
-
-    let rustls_config = ServerConfig::builder()
-        .with_safe_defaults()
-        .with_no_client_auth()
-        .with_cert_resolver(state.resolver());
-
-    let acceptor = state.axum_acceptor(Arc::new(rustls_config));
-
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-
     let app = Router::new()
         .nest(
             &format!("/api/v{}", &env!("CARGO_PKG_VERSION")[..1]),
@@ -34,9 +20,23 @@ async fn main() {
         )
         .layer(cors());
 
-    axum_server::bind(addr)
-        .acceptor(acceptor)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    match RustlsConfig::from_pem_file(
+        "examples/self-signed-certs/cert.pem",
+        "examples/self-signed-certs/key.pem",
+    )
+    .await
+    {
+        Ok(config) => {
+            axum_server::bind_rustls(addr, config)
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        }
+        Err(_) => {
+            axum_server::bind(addr)
+                .serve(app.into_make_service())
+                .await
+                .unwrap();
+        }
+    }
 }
