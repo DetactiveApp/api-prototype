a5beb7d01821da7cc45bc0dd9f0e72dccdba43d6
Maccci <max@maxbeier.dev>
2023-05-06 11:45:20 +0200

    Update init.sh

--- gateway/init.sh
+++ gateway/init.sh
@@ -39,7 +39,7 @@ echo
 
 
 echo "### Starting nginx ..."
-docker-compose up --force-recreate -d gateway
+docker-compose up --force-recreate -d gateway --build
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
