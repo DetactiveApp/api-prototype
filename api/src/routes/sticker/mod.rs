93b81345a16ca9602d0069de80345758dc9fadb7
Maccci <max@maxbeier.dev>
2023-05-13 20:59:48 +0200

    remove test

--- api/src/routes/sticker/mod.rs
+++ api/src/routes/sticker/mod.rs
@@ -3,12 +3,6 @@ use axum::{routing::get, Router};
 mod var_id;
 use var_id::request;
 
-async fn test() -> &'static str {
-    return "yooooo";
-}
-
 pub async fn sticker_router() -> Router {
-    return Router::new()
-        .route("/:id", get(request))
-        .route("/test", get(test));
+    return Router::new().route("/:id", get(request));
 }
