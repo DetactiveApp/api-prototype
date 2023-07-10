2f7fa174c40d31d605a52f59124e4d880027ec10
Max-Beier <max@maxbeier.dev>
2023-07-10 11:21:44 +0200

    fixed list.rs

--- api/src/routes/game/list.rs
+++ api/src/routes/game/list.rs
@@ -1,7 +1,7 @@
 use axum::{extract::Query, Extension, Json};
 use reqwest::StatusCode;
 use serde::{Deserialize, Serialize};
-use sqlx::Row;
+use sqlx::{postgres::PgRow, Row};
 use uuid::Uuid;
 
 use crate::{types::ApiContext, utils::geo::get_tags};
@@ -37,9 +37,11 @@ pub async fn get_request(
         .map(|row| row.get("uuid"))
         .collect();
 
+    println!("Story Uuids: {:?}", story_uuids);
+
     for story_uuid in &story_uuids {
-        let story_waypoints: Vec<Uuid> = sqlx::query(
-            "SELECT waypoint_uuid FROM steps WHERE story_uuid = $1  AND waypoint_uuid IS NOT null;",
+        let waypoint_uuids: Vec<Uuid> = sqlx::query(
+            "SELECT waypoint_uuid FROM steps WHERE story_uuid = $1 AND waypoint_uuid IS NOT null;",
         )
         .bind(story_uuid)
         .fetch_all(&ctx.detactive_db)
@@ -49,15 +51,19 @@ pub async fn get_request(
         .map(|row| row.get("waypoint_uuid"))
         .collect();
 
+        println!("Waypoints Uuids: {:?}", waypoint_uuids);
+
         let mut story_tags: Vec<String> = vec![];
-        for story_waypoint in &story_waypoints {
+        for waypoint_uuid in &waypoint_uuids {
             story_tags.push(
-                sqlx::query("SELECT place_type FROM waypoints WHERE uuid = $1 AND place_override = false AND place_type != 'random';")
-                    .bind(story_waypoint)
+                match sqlx::query("SELECT place_type FROM waypoints WHERE uuid = $1 AND place_override = false AND place_type != 'random';")
+                    .bind(waypoint_uuid)
                     .fetch_one(&ctx.detactive_db)
                     .await
-                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-                    .get("place_type"),
+                    {
+                        Ok(row) => row.get("place_type"),
+                        Err(_) => String::new(),
+                    }
             );
         }
 
