21f3d4f2e377582ba7e3ae66d4c9c12bd5520af3
Max-Beier <max@maxbeier.dev>
2023-07-10 14:40:35 +0200

    New forward logic

--- api/src/routes/game/forward.rs
+++ api/src/routes/game/forward.rs
@@ -8,7 +8,7 @@ use sqlx::Row;
 use uuid::Uuid;
 
 use crate::{
-    types::{ApiContext, DDecision, DStep, DWaypoint},
+    types::{ApiContext, DCoord, DDecision, DStep, DWaypoint},
     utils::geo::near,
 };
 
@@ -21,16 +21,108 @@ pub struct QueryParams {
 
 pub async fn get_request(
     Path(user_story_uuid): Path<Uuid>,
-    Query(mut params): Query<QueryParams>,
+    Query(params): Query<QueryParams>,
     Extension(ctx): Extension<ApiContext>,
 ) -> Result<Json<DStep>, StatusCode> {
-    let first_step: bool =
-        sqlx::query("SELECT 1 FROM user_story_steps WHERE user_story_uuid = $1;")
-            .bind(user_story_uuid)
-            .fetch_one(&ctx.detactive_db)
+    let first_step = sqlx::query("SELECT * FROM user_story_steps WHERE user_story_uuid = $1;")
+        .bind(user_story_uuid)
+        .fetch_one(&ctx.detactive_db)
+        .await
+        .is_ok();
+
+    if first_step {
+        let story_uuid: String =
+            sqlx::query("SELECT story_uuid FROM user_stories WHERE uuid = $1;")
+                .bind(user_story_uuid)
+                .fetch_one(&ctx.detactive_db)
+                .await
+                .map_err(|_| StatusCode::NOT_FOUND)?
+                .get("uuid");
+
+        let step_uuids: Vec<String> = sqlx::query("SELECT uuid FROM steps WHERE story_uuid = $1;")
+            .bind(story_uuid)
+            .fetch_all(&ctx.detactive_db)
             .await
-            .unwrap()
-            .get("1");
+            .map_err(|_| StatusCode::NOT_FOUND)?
+            .iter()
+            .map(|row| row.get("uuid"))
+            .collect();
+
+        for step_uuid in &step_uuids {
+            let decision: Option<DDecision> =
+                match sqlx::query("SELECT * FROM decisions WHERE step_output_uuid = $1;")
+                    .bind(step_uuid)
+                    .fetch_one(&ctx.detactive_db)
+                    .await
+                {
+                    Ok(row) => Some(DDecision {
+                        uuid: row.get("uuid"),
+                        step_input_uuid: row.get("step_input_uuid"),
+                        step_output_uuid: row.get("step_output_uuid"),
+                        title: row.get("title"),
+                    }),
+                    Err(_) => None,
+                };
+
+            if decision.is_some() {
+                let next_decisions: Vec<DDecision> =
+                    sqlx::query("SELECT * FROM decisions WHERE step_input_uuid = $1;")
+                        .bind(step_uuid)
+                        .fetch_all(&ctx.detactive_db)
+                        .await
+                        .unwrap_or(vec![])
+                        .iter()
+                        .map(|row| DDecision::from(row))
+                        .collect();
+
+                let waypoint_uuid: Option<String> =
+                    sqlx::query("SELECT waypoint_uuid FROM steps WHERE uuid = $1;")
+                        .bind(step_uuid)
+                        .fetch_one(&ctx.detactive_db)
+                        .await
+                        .unwrap()
+                        .get("waypoint_uuid");
+
+                let waypoint: Option<DWaypoint> =
+                    sqlx::query("SELECT uuid FROM waypoints WHERE uuid = $1")
+                        .bind(&waypoint_uuid)
+                        .fetch_one(&ctx.detactive_db)
+                        .await
+                        .map(|row: sqlx::postgres::PgRow| DWaypoint {
+                            uuid: row.get("uuid"),
+                            coordinates: DCoord { lat: 0.0, lon: 0.0 },
+                        })
+                        .ok()
+                        .into();
+
+                let waypoint_tag: Option<String> =
+                    sqlx::query("SELECT place_type FROM waypoints WHERE uuid = $1")
+                        .bind(&waypoint_uuid)
+                        .fetch_one(&ctx.detactive_db)
+                        .await
+                        .map(|row: sqlx::postgres::PgRow| row.get("place_type"))
+                        .ok();
+
+                let coordinate: DCoord = near(waypoint_tag.unwrap(), params.lat, params.lon)
+                    .await
+                    .unwrap();
+
+                let waypoint: Option<DWaypoint> = Some(DWaypoint {
+                    uuid: waypoint.unwrap().uuid,
+                    coordinates: coordinate,
+                });
+
+                return match sqlx::query("SELECT * FROM steps WHERE uuid = $1;")
+                    .bind(step_uuid)
+                    .fetch_one(&ctx.detactive_db)
+                    .await
+                {
+                    Ok(row) => Ok(Json(DStep::from(&row, next_decisions, waypoint))),
+                    Err(_) => Err(StatusCode::NOT_FOUND),
+                };
+            }
+        }
+    }
 
     Ok(Json(DStep {
         uuid: todo!(),
--- api/src/types/dtypes.rs
+++ api/src/types/dtypes.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{postgres::PgRow, Row};
 use uuid::Uuid;
 
 use super::MediaType;
@@ -18,9 +19,20 @@ pub struct DWaypoint {
 #[derive(Serialize, Deserialize)]
 pub struct DDecision {
     pub uuid: Uuid,
-    pub step_input_uuid: Uuid,
-    pub step_output_uuid: Uuid,
+    pub step_input_uuid: Option<Uuid>,
+    pub step_output_uuid: Option<Uuid>,
     pub title: String,
+}
+
+impl DDecision {
+    pub fn from(row: &PgRow) -> Self {
+        Self {
+            uuid: row.get("uuid"),
+            step_input_uuid: row.get("step_input_uuid"),
+            step_output_uuid: row.get("step_output_uuid"),
+            title: row.get("title"),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -32,4 +44,18 @@ pub struct DStep {
     pub title: String,
     pub decisions: Vec<DDecision>,
     pub waypoint: Option<DWaypoint>,
+}
+
+impl DStep {
+    pub fn from(row: &PgRow, decisions: Vec<DDecision>, waypoint: Option<DWaypoint>) -> Self {
+        Self {
+            uuid: row.get("uuid"),
+            description: row.get("description"),
+            media_type: row.get("media_type"),
+            src: row.get("src"),
+            title: row.get("title"),
+            decisions: decisions,
+            waypoint: waypoint,
+        }
+    }
 }
