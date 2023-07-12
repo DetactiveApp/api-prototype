d0e43426200c9ed4a48527221d5d682b67064ed8
Max-Beier <max@maxbeier.dev>
2023-07-12 13:11:37 +0200

    Fixed near function

--- api/src/utils/geo/near.rs
+++ api/src/utils/geo/near.rs
@@ -13,62 +13,62 @@ pub async fn near(
     lon: f64,
     place_override: bool,
 ) -> Result<DCoord, StatusCode> {
+    let mut features: HashMap<String, DCoord> = HashMap::new();
     let mapbox_token = &env::var("MAPBOX_TOKEN").expect("Mapbox access token not found.");
 
     // Check if the tag is "random" and returns a random user accessible coordinate
     if tag == "random" {
         return random_features(lat, lon, mapbox_token).await;
-    }
+    } else {
+        // Filters tags out of the requests for each quad coordinates
+        for coord in latlon::quad(lat, lon, 500.0).iter() {
+            let lon: &f64 = coord.get(1).unwrap();
+            let lat: &f64 = coord.get(0).unwrap();
 
-    // Filters tags out of the requests for each quad coordinates
-    let mut features: HashMap<String, DCoord> = HashMap::new();
-    for coord in latlon::quad(lat, lon, 500.0).iter() {
-        let lon: &f64 = coord.get(1).unwrap();
-        let lat: &f64 = coord.get(0).unwrap();
+            let url = format!(
+                    "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/{lon},{lat}.json?radius=1000&limit=50&layers=poi_label&access_token={mapbox_token}"
+            );
 
-        let url = format!(
-                "https://api.mapbox.com/v4/mapbox.mapbox-streets-v8/tilequery/{lon},{lat}.json?radius=1000&limit=50&layers=poi_label&access_token={mapbox_token}"
-        );
+            let response = reqwest::get(&url)
+                .await
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+                .json::<serde_json::Value>()
+                .await
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-        let response = reqwest::get(&url)
-            .await
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-            .json::<serde_json::Value>()
-            .await
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+            response
+                .get("features")
+                .and_then(|_features| _features.as_array())
+                .unwrap()
+                .iter()
+                .for_each(|feature| {
+                    let maki = feature
+                        .get("properties")
+                        .and_then(|properties| properties.get("maki"))
+                        .and_then(|maki| maki.as_str())
+                        .unwrap();
 
-        response
-            .get("features")
-            .and_then(|_features| _features.as_array())
-            .unwrap()
-            .iter()
-            .for_each(|feature| {
-                let maki = feature
-                    .get("properties")
-                    .and_then(|properties| properties.get("maki"))
-                    .and_then(|maki| maki.as_str())
-                    .unwrap();
+                    let coordinates = feature
+                        .get("geometry")
+                        .and_then(|geometry| geometry.get("coordinates"))
+                        .and_then(|coordinates| coordinates.as_array())
+                        .and_then(|coordinates| {
+                            Some(DCoord {
+                                lat: coordinates.get(1).and_then(|lat| lat.as_f64()).unwrap(),
+                                lon: coordinates.get(0).and_then(|lon| lon.as_f64()).unwrap(),
+                            })
+                        });
 
-                let coordinates = feature
-                    .get("geometry")
-                    .and_then(|geometry| geometry.get("coordinates"))
-                    .and_then(|coordinates| coordinates.as_array())
-                    .and_then(|coordinates| {
-                        Some(DCoord {
-                            lat: coordinates.get(1).and_then(|lat| lat.as_f64()).unwrap(),
-                            lon: coordinates.get(0).and_then(|lon| lon.as_f64()).unwrap(),
-                        })
-                    });
-
-                features.insert(maki.to_string(), coordinates.unwrap());
-            });
+                    features.insert(maki.to_string(), coordinates.unwrap());
+                });
+        }
     }
 
     if features.is_empty() && place_override {
         return random_features(lat, lon, mapbox_token).await;
     }
 
-    Ok(features.get(&tag).unwrap().clone())
+    return Ok(features.get(&tag).unwrap().clone());
 }
 
 async fn random_features(lat: f64, lon: f64, mapbox_token: &String) -> Result<DCoord, StatusCode> {
