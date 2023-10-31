cbd4afe2d2abf07a778464948b021acf7e274e12
Max-Beier <max@maxbeier.dev>
2023-10-31 14:55:34 +0100

    added waypoint removal

--- api/src/routes/storystudio/story/remove.rs
+++ api/src/routes/storystudio/story/remove.rs
@@ -8,6 +8,15 @@ pub async fn remove(
     Extension(ctx): Extension<ApiContext>,
     Path(uuid): Path<Uuid>,
 ) -> Result<StatusCode, DError> {
+    sqlx::query(
+        "DELETE FROM waypoints WHERE waypoints.uuid NOT IN (
+        SELECT steps.waypoint_uuid FROM steps WHERE steps.story_uuid = $1
+    );",
+    )
+    .execute(&ctx.detactive_db)
+    .await
+    .map_err(|err| DError::from(&err.to_string(), StatusCode::INTERNAL_SERVER_ERROR))?;
+
     sqlx::query("DELETE FROM stories WHERE uuid = $1;")
         .bind(uuid)
         .execute(&ctx.detactive_db)
