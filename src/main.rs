ca5e1ffd69fd829a4f8a12b9b2b24280c4aa2c64
Max-Beier <max@maxbeier.dev>
2023-12-02 18:59:45 +0100

    added sentry

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,11 @@ async fn main() {
     dotenv().ok();
     env_logger::init();
 
+    let _guard = sentry::init(("https://d10ba764eafebb7720e11aca89b4cb79@o4506285486702592.ingest.sentry.io/4506327435182080", sentry::ClientOptions {
+        release: sentry::release_name!(),
+        ..Default::default()
+      }));
+
     let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     let app = Router::new()
         .nest(
