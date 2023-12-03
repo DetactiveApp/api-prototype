0f2fd482171d985e2ac0b280f24b50bc01c7af74
Max-Beier <max@maxbeier.dev>
2023-12-03 15:38:29 +0100

    fixed dev mode

--- development.sh
+++ development.sh
@@ -10,6 +10,5 @@ fi
 
 docker-compose --env-file .env -f docker/docker-compose.development.yml up --build -d
 
-docker system prune --force --all
-
+docker system prune --force --all --filter "until=24h"
 read -p "Press any key to continue." x
