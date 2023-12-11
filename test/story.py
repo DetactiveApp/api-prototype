2b45f24a91920f52717e4f16dad0ebd256fdb0ab
Max-Beier <max@maxbeier.dev>
2023-12-11 22:24:52 +0100

    fixed py script

--- test/story.py
+++ test/story.py
@@ -6,6 +6,13 @@ USER = "d13a9dfe-cb14-4c95-9a05-ab93eedf2e03"
 STORY = "9e125477-3a9e-4d39-ae63-5a09a50b614a"
 START_COORDS = [52.520008, 13.404954]
 
+def print_step(r_json):
+    print(f"Step: {r_json['uuid']}")
+    print(f"Title: {r_json['title']}")
+    if r_json["waypoint"]:
+        print(f"Coords: {r_json['waypoint']['coordinates']['lat']} {r_json['waypoint']['coordinates']['lon']}")
+    print("\n")
+
 # Get authentication token
 r = requests.get(URL + f"/users/{USER}/token")
 token = r.json()["token"]
@@ -22,14 +29,19 @@ data = json.dumps({
 })
 r = requests.post(URL + f"/games/{STORY}/start", data=data, headers=headers)
 step = r.json()["decisions"][0]["stepOutputUuid"]
-print(f"Next step: {step}")
+print_step(r.json())
 
 # Play story until end
 while True:
     r = requests.post(URL + f"/games/{STORY}/steps/{step}", data=data, headers=headers)
     r_json = r.json()
+    print_step(r_json)
 
     if r_json["decisions"]:
         step = r_json["decisions"][0]["stepOutputUuid"]
+       
     else:
+        print("DONE")
         break
+
+
