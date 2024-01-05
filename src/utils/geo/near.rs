13f81ededbf0880bd753eb6234f6218c0a6bb67f
Max-Beier <max@maxbeier.dev>
2024-01-05 20:38:04 +0100

    fix

--- src/utils/geo/near.rs
+++ src/utils/geo/near.rs
@@ -1,14 +1,11 @@
-use crate::{
-    types::{DCoord, DError},
-    utils::geo::{d_angle, MAX_ANGLE_TO_WAYPOINT},
-};
+use crate::types::{DCoord, DError};
 use rand::seq::SliceRandom;
 use reqwest::{self, StatusCode};
 use std::env::{self};
 
 use super::MAX_MINUTES_TO_WAYPOINT;
 
-pub async fn near(origin: &DCoord, angle: f64) -> Result<Option<DCoord>, DError> {
+pub async fn near(origin: &DCoord, _angle: f64) -> Result<Option<DCoord>, DError> {
     let mapbox_token = &env::var("MAPBOX_TOKEN").expect("Mapbox access token not found.");
     let url = format!(
         "https://api.mapbox.com/isochrone/v1/{profile}/{lon},{lat}?contours_minutes={minutes}&access_token={access_token}",
@@ -38,7 +35,7 @@ pub async fn near(origin: &DCoord, angle: f64) -> Result<Option<DCoord>, DError>
 
     let mut rng = rand::thread_rng();
 
-    let mut coordinates: Vec<DCoord> = response
+    let coordinates: Vec<DCoord> = response
         .get("features")
         .and_then(|value| value.as_array().unwrap().choose(&mut rng))
         .and_then(|value| value.get("geometry"))
@@ -50,15 +47,6 @@ pub async fn near(origin: &DCoord, angle: f64) -> Result<Option<DCoord>, DError>
             lat: c.get(1).unwrap().as_f64().unwrap(),
             lon: c.get(0).unwrap().as_f64().unwrap(),
         })
-        .collect();
-
-    coordinates = coordinates
-        .iter()
-        .filter(|c| {
-            (d_angle(origin, c).to_radians() + angle.to_radians()).to_degrees()
-                < MAX_ANGLE_TO_WAYPOINT
-        })
-        .cloned()
         .collect();
 
     Ok(coordinates.choose(&mut rng).cloned())
